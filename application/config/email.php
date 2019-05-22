6cc4dcf1649b3920b5a276775b1943dad0727887
Márcio Fernando Santos <marciofernandonet@hotmail.com>
2019-05-21 22:01:25 -0300

    Ok

---
+++ application/config/email.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = [        
+    'protocol' => 'smtp',
+    'smtp_host' => 'smtp.office365.com',
+    'smtp_user' => 'marciofernandonet@hotmail.com',
+    'smtp_pass' => '',
+    'smtp_crypto' => 'tls',    
+    'newline' => "\r\n",
+    'smtp_port' => 587,
+    'mailtype' => 'html',
+
+    'email_to' => 'marciofernandonet@gmail.com', //E-mail de destino
+    'email_subject' => 'Dados de Contatos' //Assunto
+];
